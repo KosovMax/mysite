79980db66b3a9a9b755ebc9751126c812ce7524b
KosovServer <kosovserver@kosovserver.com.ua>
2016-02-16 18:24:15 +0200

    Task Yii2

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=mysite',
     'username' => 'root',
-    'password' => 'moskvich408',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
